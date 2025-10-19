a1b74f59bf0f5899d797fdaf3d2b95a4650f2ffb
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-10-19 04:28:05 +0000

    domingos de luto

---
+++ infra/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+region = var.aws_region
+}
+
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh-access"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # substitua pelo seu IP público
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "lab" {
+bucket = var.bucket_name
+}
+resource "aws_s3_bucket_versioning" "versioning" {
+bucket = aws_s3_bucket.lab.id
+versioning_configuration {
+status = "Enabled"
+}
+}
+resource "aws_instance" "app_server" {
+ami = "ami-0c02fb55956c7d316" # Amazon Linux 2
+instance_type = "t2.micro"
+key_name = "control-node"
+tags = {
+Name = "Sprint5-EC2"
+}
+}
+output "bucket_name" {
+value = aws_s3_bucket.lab.bucket
+}
+output "ec2_public_ip" {
+value = aws_instance.app_server.public_ip
+}
+
+
